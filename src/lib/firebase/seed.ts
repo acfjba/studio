87b22e45d4104d1afc17678611cae9752b060e3f
harekrishna cloud <cloudharekrishna@gmail.com>
2025-07-20 06:13:48 +0000

    https://console.cloud.google.com/firestore/databases/-default-/data/pane

--- src/lib/firebase/seed.ts
+++ src/lib/firebase/seed.ts
@@ -1,5 +1,5 @@
 // src/lib/firebase/seed.ts
-import { writeBatch, collection, doc } from 'firebase/firestore';
+import { writeBatch, doc } from 'firebase/firestore';
 import { db, isFirebaseConfigured } from './config';
 import { staffData as sampleStaffSeedData, schoolData, sampleLibraryBooksData, sampleExamResultsData } from '@/lib/data';
 
@@ -11,30 +11,41 @@ export async function seedDatabase() {
   const batch = writeBatch(db);
 
   // Seed Schools
+  console.log("Seeding schools...");
   schoolData.forEach(school => {
     const docRef = doc(db, 'schools', school.id);
     batch.set(docRef, school);
   });
 
   // Seed Staff
+  console.log("Seeding staff...");
   sampleStaffSeedData.forEach(staff => {
+    // The 'id' from the seed data will be used as the document ID in Firestore.
     const { id, ...staffDetails } = staff;
-    const docRef = doc(db, 'staff', id);
-    batch.set(docRef, staffDetails);
+    if (id) {
+      const docRef = doc(db, 'staff', id);
+      batch.set(docRef, staffDetails);
+    }
   });
 
   // Seed Library Books
+  console.log("Seeding library books...");
   sampleLibraryBooksData.forEach(book => {
-      const { id, ...bookDetails } = book;
-      const docRef = doc(db, 'books', id);
-      batch.set(docRef, bookDetails);
+    const { id, ...bookDetails } = book;
+    if (id) {
+        const docRef = doc(db, 'books', id);
+        batch.set(docRef, bookDetails);
+    }
   });
 
   // Seed Exam Results
+  console.log("Seeding exam results...");
   sampleExamResultsData.forEach(result => {
       const { id, ...resultDetails } = result;
-      const docRef = doc(db, 'examResults', id);
-      batch.set(docRef, resultDetails);
+      if (id) {
+        const docRef = doc(db, 'examResults', id);
+        batch.set(docRef, resultDetails);
+      }
   });
   
   // You can add more collections to seed here, for example:
