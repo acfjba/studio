b7639ec533a99f5c6467e2daf3beb9ef7e9121b2
harekrishna cloud <cloudharekrishna@gmail.com>
2025-07-21 01:49:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -147,7 +147,7 @@ export function Header() {
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-               <Link href="/dashboard" legacyBehavior passHref>
+               <Link href="/dashboard" passHref>
                  <NavigationMenuLink active={pathname === '/dashboard'} className={navigationMenuTriggerStyle()}>
                     Dashboard
                  </NavigationMenuLink>
