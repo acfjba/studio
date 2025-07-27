b1106a86980c8a062b3da78695fcc3872f28de20
harekrishna cloud <cloudharekrishna@gmail.com>
2025-07-27 19:32:02 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // This is required to allow the Next.js dev server to be accessed from
-    // the Cloud Workstation and other development environments.
-    allowedDevOrigins: [
-        "*.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev",
-        "*.cloudworkstations.dev"
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
